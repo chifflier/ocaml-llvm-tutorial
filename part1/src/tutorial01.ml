5880cee1ef2554ed11a7a54520c57d12479e17a1
Pollux <pollux@wzdftpd.net>
2015-06-08 23:29:45 +0200

    Tutorial part 1: Makefile, read bitcode, dump LLVM IR

---
+++ part1/src/tutorial01.ml
@@ -0,0 +1,6 @@
+let _ =
+  let llctx = Llvm.global_context () in
+  let llmem = Llvm.MemoryBuffer.of_file Sys.argv.(1) in
+  let llm = Llvm_bitreader.parse_bitcode llctx llmem in
+  Llvm.dump_module llm ;
+  ()
