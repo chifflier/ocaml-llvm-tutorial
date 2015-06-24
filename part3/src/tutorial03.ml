799c31940bf3e099b0543e475a8dddf1b8500779
Pollux <pollux@wzdftpd.net>
2015-06-24 16:48:45 +0200

    Tutorial part 3: creating and compiling LLVM bitcode

---
+++ part3/src/tutorial03.ml
@@ -0,0 +1,16 @@
+open Llvm
+
+let _ =
+  let llctx = global_context () in
+  let llm = create_module llctx "mymodule" in
+
+  let i32_t = i32_type llctx in
+  let fty = function_type i32_t [| |] in
+
+  let f = define_function "main" fty llm in
+  let llbuilder = builder_at_end llctx (entry_block f) in
+
+  let _ = build_ret (const_int i32_t 0) llbuilder in
+
+  dump_module llm ;
+  ()
